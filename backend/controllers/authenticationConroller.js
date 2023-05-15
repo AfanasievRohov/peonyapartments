5ce847a04ba89aef8c3a13a1fa2acf9229747935
Andrii Rohov <andrik.la.resistance@gmail.com>
2023-05-15 16:32:16 +0300

    [WIP]backend|frontend: User signup functionality fixes

--- backend/controllers/authenticationConroller.js
+++ backend/controllers/authenticationConroller.js
@@ -1,5 +1,30 @@
 const User = require('../models/userModel');
-const catchAsync = require('../utils/catchAsync')
+const catchAsync = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken');
+
+const createAndSendToken = (user, statusCode, res) => {
+    let token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+
+    const cookieOptions = {
+        expires: new Date(Date.now() + (process.env.JWT_COOKIE_EXPIRES_IN * 24 *60 * 60 * 1000)),
+        httpOnly: true
+    };
+
+    // if (process.env.NODE_ENV === "production") cookieOptions.secure = true; //This thing blocked sending cookies if not secure (HTTPS)
+
+    res.cookie("jwt", token, cookieOptions);
+
+    user.password = undefined;
+    res.status(statusCode).json({
+        status: "succes",
+        token,
+        data: {
+            user: user
+        }
+    });
+}
 
 exports.signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create({
@@ -11,8 +36,5 @@ exports.signup = catchAsync(async (req, res, next) => {
         passwordConfirm: req.body.passwordConfirm,
     });
 
-    res.status(201).json({
-        status: "success",
-        data: newUser
-    });
+    createAndSendToken(newUser, 201, res);
 });
